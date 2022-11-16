f5093dc57a5902f545bbf296d481003033feb9b9
RotundGoose555 <115037018+RotundGoose555@users.noreply.github.com>
2022-11-15 22:41:36 -0500

    Update BakeryItem.js

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,23 @@
-// TODO: create a component that displays a single bakery item
+function BakeryItem(prop) {
+
+    const updateCart = prop.updateCart;
+    const cart = prop.cart;
+    const item = prop.item;
+    
+    const handleClick = () => {
+        console.log(prop.item.name);
+        updateCart([...cart, item]);
+    }
+
+    
+    return (
+        <div class ="items">
+            <img src={item.image}></img>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p><button onClick={handleClick}>Add to Cart</button>
+        </div>
+    );
+  }
+  
+export default BakeryItem;
